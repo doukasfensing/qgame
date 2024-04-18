7b36b10df8687c85e6b1e6cb29bc3c991490d2a0
alex <a.doukas@que-tech.com>
2024-04-18 14:20:39 +0300

    Test

--- src/data.js
+++ src/data.js
@@ -3308,55 +3308,5 @@ export const categories = [
                 ]
             }
         ],
-    },
-    {
-        id: 8,
-        name: "Science",
-        questions: [
-            {
-                questionText: "What is the chemical symbol for the element oxygen?",
-                answerOptions: [
-                    { answerText: "O", isCorrect: true },
-                    { answerText: "Ox", isCorrect: false },
-                    { answerText: "Om", isCorrect: false },
-                    { answerText: "Op", isCorrect: false }
-                ],
-            },
-            {
-                questionText: "What is the chemical symbol for the element Hydrogen?",
-                answerOptions: [
-                    { answerText: "O", isCorrect: false },
-                    { answerText: "Cx", isCorrect: false },
-                    { answerText: "Om", isCorrect: false },
-                    { answerText: "H", isCorrect: true }
-                ],
-            },
-            // Add more questions as needed
-        ],
-    },
-    {
-        id: 9,
-        name: "Science",
-        questions: [
-            {
-                questionText: "What is the chemical symbol for the element oxygen?",
-                answerOptions: [
-                    { answerText: "O", isCorrect: true },
-                    { answerText: "Ox", isCorrect: false },
-                    { answerText: "Om", isCorrect: false },
-                    { answerText: "Op", isCorrect: false }
-                ],
-            },
-            {
-                questionText: "What is the chemical symbol for the element Hydrogen?",
-                answerOptions: [
-                    { answerText: "O", isCorrect: false },
-                    { answerText: "Cx", isCorrect: false },
-                    { answerText: "Om", isCorrect: false },
-                    { answerText: "H", isCorrect: true }
-                ],
-            },
-            // Add more questions as needed
-        ],
-    },
+    }
 ];
